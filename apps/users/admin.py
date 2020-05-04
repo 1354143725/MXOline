90a3c722a49f13a52bcc3eeb3c1a2f35d6438891
1354143725 <64411974+1354143725@users.noreply.github.com>
2020-05-04 11:12:48 +0800

    xadmin的配置

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+#
+# from apps.users.models import UserProfile
+# class UserProfileAdmin(admin.ModelAdmin):
+#     pass
+# admin.site.register(UserProfile, UserProfileAdmin)
 
+#因为用户表相对来说都是系统中存在的，Django内置了一个队显示用户信息的优化
 from apps.users.models import UserProfile
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(UserProfile, UserProfileAdmin)
+from django.contrib.auth.admin import UserAdmin
+admin.site.register(UserProfile, UserAdmin)
