a069e71d0fd4006b7a35a45c6ee332bf38c1f6b1
1354143725 <64411974+1354143725@users.noreply.github.com>
2020-05-05 12:27:34 +0800

    登录优化

--- apps/users/form.py
+++ apps/users/form.py
@@ -1,6 +1,6 @@
 from django import forms
 
-
+# 抵挡用户名少于两位，密码少于3位的
 class LoginForm(forms.Form):
     username = forms.CharField(required=True, min_length=2)
     password = forms.CharField(required=True, min_length=3)
