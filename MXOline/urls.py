0874de403e5323a42f80df1e31f224c349b9c6ec
1354143725 <64411974+1354143725@users.noreply.github.com>
2020-05-07 12:15:32 +0800

    课程机构列表

--- MXOline/urls.py
+++ MXOline/urls.py
@@ -6,6 +6,9 @@ import xadmin
 from django.views.generic import TemplateView
 from apps.users.views import LoginView
 from apps.organization.views import OrgView
+from django.conf.urls import url
+from django.views.static import serve
+from MXOline.settings import MEDIA_ROOT
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('xadmin/', xadmin.site.urls),
@@ -15,5 +18,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     # 配置授课机构列表展示
     path('orglist/', OrgView.as_view(), name='orglist'),
-
+    # 配置上传文件的访问的url
+    url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
 ]
