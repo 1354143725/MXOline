3a80e2bb22a9dd279ccaa08962982ad4830a718b
1354143725 <64411974+1354143725@users.noreply.github.com>
2020-05-12 10:00:48 +0800

    url重构

--- MXOline/urls.py
+++ MXOline/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 from apps.users.views import LoginView
 from apps.courses.views import CourseView
 from apps.organization.views import OrgView
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.views.static import serve
 from MXOline.settings import MEDIA_ROOT
 urlpatterns = [
@@ -18,9 +18,12 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('login/', LoginView.as_view(), name='login'),
     # 配置授课机构列表展示
-    path('orglist/', OrgView.as_view(), name='orglist'),
+    # path('orglist/', OrgView.as_view(), name='orglist'),
     # 配置上传文件的访问的url
     url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
     path('courselist/', CourseView.as_view(), name='course-list'),
+    # 授课机构相关操作
+    url(r'^org/', include(('apps.organization.urls', 'organization'), namespace='org')),
+
 
 ]
---
+++ apps/organization/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url,include
+from apps.organization.views import OrgView
+urlpatterns = [
+    url(r'^list/$', OrgView.as_view(), name='list'),
+]
