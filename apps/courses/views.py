132d8fc397a3873d9bdec543e6cb37f13b6bf936
1354143725 <64411974+1354143725@users.noreply.github.com>
2020-05-13 09:49:20 +0800

    课程显示，排序，热门

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -16,15 +16,27 @@ class CourseListView(View):
         """
         """获取课程列表信息"""
         all_courses = Course.objects.order_by("-add_time")
+        hot_orgs = all_courses.order_by("-click_nums")[:3]
+
+        sort = request.GET.get('sort', "")
+        if sort == 'students':
+            # 根据学生人数排序  减号代表倒序排序的意思
+            all_courses = all_courses.order_by('-students')
+        elif sort == 'hot':
+            # 根据课程数进行排序
+            all_courses = all_courses.order_by('-fav_nums')
         # 对课程机构进行分页
         try:
             page = request.GET.get('page', 1)
         except PageNotAnInteger:
             page = 1
 
-        p = Paginator(all_courses, per_page=3, request=request)  # 每页显示多少个per_page
+        p = Paginator(all_courses, per_page=10, request=request)  # 每页显示多少个per_page
         courses = p.page(page)
 
         return render(request, 'course-list.html',
                       {"all_courses": courses,
+                      "sort": sort,
+                      "hot_orgs": hot_orgs,
+
                        })
