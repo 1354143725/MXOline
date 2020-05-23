5e0d2670f104f15ba4cb8e3f52ec921bc46f2e04
1354143725 <64411974+1354143725@users.noreply.github.com>
2020-05-23 12:00:41 +0800

    个人中心其他的显示以及我的课程的动态显示

--- apps/users/views.py
+++ apps/users/views.py
@@ -57,7 +57,6 @@ class UserInfoView(LoginRequiredMixin, View):
     login_url = '/login/'
     def get(self, request, *args, **kwargs):
         current_page = 'info'
-
         return render(request, 'usercenter-info.html',{
             "current_page":current_page
         })
